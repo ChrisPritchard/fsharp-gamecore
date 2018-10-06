0b7392c20f3722f32af60315c2bbedab4950ebbb
Christopher <chrispritchard.nz@gmail.com>
2018-10-07 12:49:52 +1300

    Added working sample project

---
+++ sample/Program.fs
@@ -0,0 +1,13 @@
+﻿open GameCore
+open Model
+
+[<EntryPoint>]
+let main _ =
+    
+    let resolution = Windowed (640, 480)
+    let updateModel model runState = Some 0
+    let getView model runState = []
+
+    use game = new GameLoop<int>(resolution, [], updateModel, getView, true)
+    game.Run ()
+    0
