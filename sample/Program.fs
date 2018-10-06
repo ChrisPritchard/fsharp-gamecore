9c062d2231964729b0e1a40e9d75c33a0d4e9f2b
Christopher <chrispritchard.nz@gmail.com>
2018-10-07 12:56:16 +1300

    Added the ability to exit, with comments

--- sample/Program.fs
+++ sample/Program.fs
@@ -1,11 +1,21 @@
 ﻿open GameCore.GameLoop
 open GameCore.Model
+open Microsoft.Xna.Framework.Input
 
 [<EntryPoint>]
 let main _ =
     
     let resolution = Windowed (640, 480)
-    let updateModel model runState = Some 0
+
+    let updateModel runState model =
+        match model with
+        // The model will start as none, so this is where the initial model is specified
+        | None -> Some 0
+        // returnin None from an updateModel function will exit the application
+        | _ when wasJustPressed Keys.Escape runState -> None
+        // business as usual
+        | Some n -> Some <| n + 1
+
     let getView model runState = []
 
     use game = new GameLoop<int>(resolution, [], updateModel, getView, true)
