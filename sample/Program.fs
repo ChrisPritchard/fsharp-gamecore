9b436bdda7d484db4fec8b1b4f6814a1614be025
Christopher <chrispritchard.nz@gmail.com>
2018-10-08 09:29:51 +1300

    Added some sample display content

--- sample/Program.fs
+++ sample/Program.fs
@@ -1,11 +1,13 @@
 ﻿open GameCore.GameLoop
 open GameCore.Model
+open Microsoft.Xna.Framework
 open Microsoft.Xna.Framework.Input
 
 [<EntryPoint>]
 let main _ =
     
-    let resolution = Windowed (640, 480)
+    let (width, height) = 640, 480
+    let resolution = Windowed (width, height)
 
     // a list of GameCore.Model.Loadable values
     let assetsToLoad = []
@@ -20,8 +22,15 @@ let main _ =
         | Some n -> Some <| n + 1
 
     // should return a list of GameCore.Model.ViewArtifacts
-    let getView model runState = []
+    let getView runState model = 
+        let (centrex, centrey) = width/2, height/2
+        [
+            Colour ((centrex - 100, centrey - 50, 200, 80), Color.Red)
+            // just rendering the model (an ever increasing int) centre screen
+            SystemText (sprintf "%i" model, (centrex, centrey), Centre, 1., Color.White)
+        ]
 
-    use game = new GameLoop<int>(resolution, assetsToLoad, updateModel, getView, true)
+    let showFPS = true // this will have FPS rendered in the top right, topping out at about 60 if all is well.
+    use game = new GameLoop<int>(resolution, assetsToLoad, updateModel, getView, showFPS)
     game.Run ()
     0
